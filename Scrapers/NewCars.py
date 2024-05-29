a73ab50787e11b48646f1884601145199c64869b
Mousser007 <93079943+Mousser007@users.noreply.github.com>
2024-05-29 17:43:34 +0100

    Update NewCars.py

--- Scrapers/NewCars.py
+++ Scrapers/NewCars.py
@@ -5,7 +5,10 @@ from CleaningProcess import CleaningNewCars
 
 if __name__ == "__main__":
     test = ScrapperAutomobileTnNeuf()
-    test.run_whole_process()
+    test2 = ScrapperAutomobileTnOcc()
+    soup = test2.parsing_page_source('https://www.automobile.tn/fr/neuf/audi')
+    print(soup)
+    # test.run_whole_process()
     # test = ScrappAutoPlusTnNeuf()
     # test.run_whole_process()
     # cleaningNewCars = CleaningNewCars()
