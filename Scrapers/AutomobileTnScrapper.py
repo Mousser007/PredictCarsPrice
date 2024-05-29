97e72495926b4f5ced72e9e85b2e56ebbedb2591
Mousser007 <93079943+Mousser007@users.noreply.github.com>
2024-05-29 15:33:44 +0100

    Update AutomobileTnScrapper.py

--- Scrapers/AutomobileTnScrapper.py
+++ Scrapers/AutomobileTnScrapper.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 from Cleaning.ColumnStandardiser import ColumnsStandardiser
