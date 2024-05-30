6fbc2a879b5e39d6f89d397f2566ad0e54dc5793
Mousser007 <93079943+Mousser007@users.noreply.github.com>
2024-05-30 17:47:28 +0100

    Update NewCars.py

--- Scrapers/NewCars.py
+++ Scrapers/NewCars.py
@@ -7,7 +7,7 @@ from CleaningProcess import CleaningNewCars
 if __name__ == "__main__":
     test = ScrapperAutomobileTnNeuf()
     test.run_whole_process()
-    # test = ScrappAutoPlusTnNeuf()
-    # test.run_whole_process()
+    test = ScrappAutoPlusTnNeuf()
+    test.run_whole_process()
     # cleaningNewCars = CleaningNewCars()
     # cleaningNewCars.cleaning()
