835fa95fff04f72c818767e28d08d7467628b7ca
Mousser Selmi <mousser.selmi@esprit.tn>
2024-05-31 13:23:48 +0100

    commit 31/05

--- Scrapers/NewCars.py
+++ Scrapers/NewCars.py
@@ -5,9 +5,9 @@ from CleaningProcess import CleaningNewCars
 
 
 if __name__ == "__main__":
-    test = ScrapperAutomobileTnNeuf()
-    test.run_whole_process()
-    test = ScrappAutoPlusTnNeuf()
-    test.run_whole_process()
-    # cleaningNewCars = CleaningNewCars()
-    # cleaningNewCars.cleaning()
+    # test = ScrapperAutomobileTnNeuf()
+    # test.run_whole_process()
+    # test = ScrappAutoPlusTnNeuf()
+    # test.run_whole_process()
+    cleaningNewCars = CleaningNewCars()
+    cleaningNewCars.cleaning()
