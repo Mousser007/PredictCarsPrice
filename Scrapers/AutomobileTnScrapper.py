a3b5f3ffbe4fcaba07d7bfd64f1e6f2872def43d
Mousser007 <93079943+Mousser007@users.noreply.github.com>
2024-06-01 20:37:11 +0100

    Update AutomobileTnScrapper.py

--- Scrapers/AutomobileTnScrapper.py
+++ Scrapers/AutomobileTnScrapper.py
@@ -36,7 +36,7 @@ class ScrapperAutomobileTnOcc:
     def parsing_page_source(self, url):
         try:
             self.driver.get(url)
-            time.sleep(20)
+            time.sleep(25)
         except WebDriverException:
             self.driver.refresh()
             time.sleep(20)
