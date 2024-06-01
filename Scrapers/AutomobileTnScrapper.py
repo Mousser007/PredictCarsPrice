da96161e0426d546656ab5851383587a2c832729
Mousser007 <93079943+Mousser007@users.noreply.github.com>
2024-06-01 19:10:02 +0100

    Update AutomobileTnScrapper.py

--- Scrapers/AutomobileTnScrapper.py
+++ Scrapers/AutomobileTnScrapper.py
@@ -36,10 +36,10 @@ class ScrapperAutomobileTnOcc:
     def parsing_page_source(self, url):
         try:
             self.driver.get(url)
-            time.sleep(4)
+            time.sleep(20)
         except WebDriverException:
             self.driver.refresh()
-            time.sleep(4)
+            time.sleep(20)
         return BeautifulSoup(self.driver.page_source,'html.parser') if BeautifulSoup(self.driver.page_source,'html.parser') else None
     
     def extract_cars_urls(self, pageUrl):
